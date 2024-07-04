ed53569a58afb2df151648243466ddbc45db8cda
M Tayyab Asghar <muhammadtayyabasghar@gmail.com>
2024-07-04 13:02:27 +0500

    ✨ NEW-FEATURE: Completed Log In functionality.

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,16 @@
 import bcrypt from "bcryptjs";
 import { Router } from "express";
+import JWT from "jsonwebtoken";
 import User from "../models/user.js";
 
 const router = Router();
 
-/* SignUP */
 router.post("/signup", async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
         /* Check if user exists */
-        const existingUser = await findOne({ email });
+        const existingUser = await User.findOne({ email });
         if (existingUser) return res.status(409).json({ message: "User already exists!" });
 
         /* Hash the password */
@@ -30,6 +30,27 @@ router.post("/signup", async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: err.message });
+    }
+}).post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        /* Check if user exists */
+        const user = await User.findOne({ email });
+        if (!user) return res.status(409).json({ message: "User doesn't exist!" });
+
+        /* Compare the password with the hashed password */
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Invalid Credentials!" });
+
+        /* Generate JWT token */
+        const token = JWT.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+
+        res.status(200).json({ token, user });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
     }
 });
 
