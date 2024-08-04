39d20527d200c24763852b0162edaf92f64c3d9f
M Tayyab Asghar <muhammadtayyabasghar@gmail.com>
2024-08-04 11:35:05 +0500

    🐛 FIX: Trying to serve favicon from server side.

--- server/controllers/base.controller.js
+++ server/controllers/base.controller.js
@@ -3,7 +3,3 @@ import asyncHandler from "../libs/asyncHandler.js";
 export const handleRootRequest = asyncHandler(async (_, res) =>
     res.status(200).send('Server is up and running')
 );
-
-export const getFavIcon = asyncHandler(async (_, res) =>
-    res.status(200).send('Server is up and running')
-);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import cors from "cors";
+import { join } from "path";
 import express from "express";
 import { config } from "dotenv";
+import favicon from "serve-favicon";
 import cookieParser from "cookie-parser";
 import connectDB from "./database/index.js";
 import authRouter from "./routes/auth.routes.js";
@@ -11,10 +13,14 @@ import listingRouter from "./routes/listing.routes.js";
 
 config();
 const app = express();
+
 app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use(favicon(join("public", "favicon.ico"), {
+    maxAge: process.env.JWT_REFRESH_TOKEN_EXPIRY
+}));
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_BASE_URL
