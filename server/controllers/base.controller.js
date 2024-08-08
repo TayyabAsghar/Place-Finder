f379dda5e707df70cabc9ac6a73e4206c2d8ad52
M Tayyab Asghar <muhammadtayyabasghar@gmail.com>
2024-08-08 06:52:13 +0500

    🐛 FIX: Serving static files from vercel and express.

--- server/controllers/base.controller.js
+++ server/controllers/base.controller.js
@@ -1,17 +1,5 @@
-import { parse, join } from "path";
 import asyncHandler from "../libs/asyncHandler.js";
 
 export const handleRootRequest = asyncHandler(async (_, res) =>
     res.status(200).send('Server is up and running')
 );
-
-export const handleFaviconRequest = asyncHandler(async (_, res) => {
-    const __dirname = parse(import.meta.dirname).dir;
-    const fileDirectory = join(__dirname, "public");
-
-    res.sendFile("favicon.ico", { root: fileDirectory }, err => {
-        console.log(fileDirectory);
-        res.end();
-        if (err) throw err;
-    });
-});
--- server/routes/base.routes.js
+++ server/routes/base.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { handleRootRequest, handleFaviconRequest } from "../controllers/base.controller.js";
+import { handleRootRequest } from "../controllers/base.controller.js";
 
 const router = Router();
 
 router.get("", handleRootRequest);
 router.get("/heart", handleRootRequest);
-// router.get("/favicon.ico", handleFaviconRequest);
 
 export default router;
