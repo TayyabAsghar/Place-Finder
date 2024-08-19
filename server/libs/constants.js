1eda57d56e3ba343f52a924afd35e78ccc62eabe
M Tayyab Asghar <muhammadtayyabasghar@gmail.com>
2024-08-19 17:26:53 +0500

    🐛 FIX: Cookies samesite check.

--- server/libs/constants.js
+++ server/libs/constants.js
@@ -6,7 +6,7 @@ export const options = {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 365,  // 1 year
     secure: process.env.ENV === "PROD",
-    sameSite: process.env.ENV === "PROD" ? "none" : "lax",
+    sameSite: process.env.ENV === "PROD" ? "strict" : "lax",
     domain: process.env.ENV === "PROD" ? process.env.CLIENT_BASE_URL.split("://")[1] : "localhost"
 };
 
